04e9a16cf2a01ef7a1d2a244058398336332c8b6
fiba-dev <fibarrola06@gmail.com>
2023-12-15 18:18:34 -0400

    fix menu mobile

--- src/app/components/ui/particles/index.tsx
+++ src/app/components/ui/particles/index.tsx
@@ -5,25 +5,25 @@ import { loadSlim } from "tsparticles-slim";
 import "./index.css";
 
 export const Particulas = () => {
-	const particlesInit = useCallback(async (engine: any) => {
-		console.log(engine);
-		// you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-		// this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-		// starting from v2 you can add only the features you need reducing the bundle size
-		//await loadFull(engine);
-		await loadSlim(engine);
-	}, []);
+	// const particlesInit = useCallback(async (engine: any) => {
+	// 	console.log(engine);
+	// 	// you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+	// 	// this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+	// 	// starting from v2 you can add only the features you need reducing the bundle size
+	// 	//await loadFull(engine);
+	// 	await loadSlim(engine);
+	// }, []);
 
-	const particlesLoaded = useCallback(async (container: any) => {
-		await console.log(container);
-	}, []);
+	// const particlesLoaded = useCallback(async (container: any) => {
+	// 	await console.log(container);
+	// }, []);
 
 	return (
 		<Particles
-			id="tsparticles"
-			url="http://foo.bar/particles.json"
-			init={particlesInit}
-			loaded={particlesLoaded}
+		// id="tsparticles"
+		// url="http://foo.bar/particles.json"
+		// init={particlesInit}
+		// loaded={particlesLoaded}
 		/>
 	);
 };
